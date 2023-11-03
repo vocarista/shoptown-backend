ae20cca934d378be182b544c9fd276f788079f32
Kumar Piyush <kr.piyush888@gmail.com>
2023-11-03 11:15:10 +0530

    jwt fix

--- src/main/java/com/shoptown/backend/databaseAndAuth/config/JwtService.java
+++ src/main/java/com/shoptown/backend/databaseAndAuth/config/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,8 +22,13 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-//    @Value("${jwt.secret}")
-    private final SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+//  @Value("${jwt.secret}")
+    private final SecretKey secretKey;
+    @Autowired
+    public JwtService() {
+        String secretKeyValue = System.getenv("JWT_SECRET");
+        this.secretKey = Keys.hmacShaKeyFor(secretKeyValue.getBytes());
+    }
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
